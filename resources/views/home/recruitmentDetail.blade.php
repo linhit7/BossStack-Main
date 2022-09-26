11c05f5f2b2a6f0a082e7eed9f9ec1b173e437ad
DuongDo1497 <113325868+DuongDo1497@users.noreply.github.com>
2022-09-26 08:05:55 +0700

    T2D

--- resources/views/home/recruitmentDetail.blade.php
+++ resources/views/home/recruitmentDetail.blade.php
@@ -8,51 +8,64 @@
       <div class="container">
         <div class="wrap">
           <h3>Frontend Developer</h3>
-          <div class="col-12 col-lg-6 work-content">
-            <div class="job-description item">
-              <h4>Mô tả công việc:</h4>
-              <ul>
-                <li>Tham gia phát triển các hệ thống website của công ty.</li>
-                <li>Tham gia phân tích, thiết kế, và cải tiến chức năng cho các sản phẩm, dịch vụ của công ty và khách hàng.</li>
-                <li>Phối hợp làm việc với team để nâng cao chất lượng công việc.</li>
-                <li>Tối ưu code để tăng tốc xử lý và mở rộng.</li>
-                <li>Tìm hiểu công nghệ theo yêu cầu của cấp trên.</li>
-                <li>Các nhiệm vụ khác theo yêu cầu của BGĐ công ty.</li>
-              </ul>
+          <div class="row">
+            <div class="col-12 col-lg-7 work-content">
+              <div class="job-description item">
+                <h4>Mô tả công việc:</h4>
+                <ul>
+                  <li>Tham gia phát triển các hệ thống website của công ty.</li>
+                  <li>Tham gia phân tích, thiết kế, và cải tiến chức năng cho các sản phẩm, dịch vụ của công ty và khách hàng.</li>
+                  <li>Phối hợp làm việc với team để nâng cao chất lượng công việc.</li>
+                  <li>Tối ưu code để tăng tốc xử lý và mở rộng.</li>
+                  <li>Tìm hiểu công nghệ theo yêu cầu của cấp trên.</li>
+                  <li>Các nhiệm vụ khác theo yêu cầu của BGĐ công ty.</li>
+                </ul>
+              </div>
+              <div class="advanced-skill item">
+                <h4>Kỹ năng & Chuyên môn:</h4>
+                <ul>
+                  <li>Có kinh nghiệm lập trình Web (HTML, CSS, Javascript).</li>
+                  <li>Thích thú với việc nghiên cứu, tìm tòi và áp dụng kỹ thuật mới.</li>
+                  <li>Chăm chỉ, chịu khó, có tinh thần chủ động học hỏi, tìm hiểu.</li>
+                  <li>Tìm hiểu các công nghệ mới, tham gia các buổi seminar của công ty.</li>
+                </ul>
+              </div>
+              <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#applyModal">
+                Ứng tuyển ngay
+              </button>
             </div>
-            <div class="advanced-skill item">
-              <h4>Kỹ năng & Chuyên môn:</h4>
-              <ul>
-                <li>Có kinh nghiệm lập trình Web (HTML, CSS, Javascript).</li>
-                <li>Thích thú với việc nghiên cứu, tìm tòi và áp dụng kỹ thuật mới.</li>
-                <li>Chăm chỉ, chịu khó, có tinh thần chủ động học hỏi, tìm hiểu.</li>
-                <li>Tìm hiểu các công nghệ mới, tham gia các buổi seminar của công ty.</li>
-              </ul>
-            </div>
-            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#applyModal">
-              Ứng tuyển ngay
-            </button>
-          </div>
-          <div class="col-12 col-lg-6 job-requirements">
-            <div class="job-description item">
-              <h4>Mô tả công việc:</h4>
-              <ul>
-                <li>Tham gia phát triển các hệ thống website của công ty.</li>
-                <li>Tham gia phân tích, thiết kế, và cải tiến chức năng cho các sản phẩm, dịch vụ của công ty và khách hàng.</li>
-                <li>Phối hợp làm việc với team để nâng cao chất lượng công việc.</li>
-                <li>Tối ưu code để tăng tốc xử lý và mở rộng.</li>
-                <li>Tìm hiểu công nghệ theo yêu cầu của cấp trên.</li>
-                <li>Các nhiệm vụ khác theo yêu cầu của BGĐ công ty.</li>
-              </ul>
-            </div>
-            <div class="advanced-skill item">
-              <h4>Kỹ năng & Chuyên môn:</h4>
-              <ul>
-                <li>Có kinh nghiệm lập trình Web (HTML, CSS, Javascript).</li>
-                <li>Thích thú với việc nghiên cứu, tìm tòi và áp dụng kỹ thuật mới.</li>
-                <li>Chăm chỉ, chịu khó, có tinh thần chủ động học hỏi, tìm hiểu.</li>
-                <li>Tìm hiểu các công nghệ mới, tham gia các buổi seminar của công ty.</li>
-              </ul>
+
+            <div class="col-12 col-lg-5 job-requirements">
+              <div class="work-location item">
+                <h4>Địa điểm</h4>
+                <ul>
+                  <li>48 Tố Hữu, Phường Trung Văn, Quận Nam Từ Liêm, Thành phố Hà Nội</li>
+                </ul>
+              </div>
+              <div class="experience item">
+                <h4>Số năm kinh nghiệm</h4>
+                <ul>
+                  <li>1 năm, 06 tháng</li>
+                </ul>
+              </div>
+              <div class="rank item">
+                <h4>Cấp bậc</h4>
+                <ul>
+                  <li>Nhân viên</li>
+                </ul>
+              </div>
+              <div class="type item">
+                <h4>Loại hình</h4>
+                <ul>
+                  <li>Full time</li>
+                </ul>
+              </div>
+              <div class="skill item">
+                <h4>Kỹ năng</h4>
+                <ul>
+                  <li>CSS, JavaScript, HTML, Jquery</li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
@@ -60,20 +73,45 @@
     </div>
   </div>
 
-  <div class="modal fade" id="applyModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal fade" id="applyModal" data-bs-backdrop="apply" data-bs-keyboard="false" tabindex="-1" aria-labelledby="applyBackdropLabel" aria-hidden="true">
     <div class="modal-dialog">
       <div class="modal-content">
-        <div class="modal-header">
-          <h4 class="modal-title" id="staticBackdropLabel">Modal title</h4>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div class="modal-body">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident et sunt doloribus iusto explicabo maiores similique animi inventore laboriosam quaerat nihil iste accusamus tenetur, repellendus modi! Delectus laudantium nostrum quod!
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-          <button type="button" class="btn btn-primary">Understood</button>
-        </div>
+        <form action="">
+          <div class="modal-body">
+            <h4>Bạn đang ứng tuyển vị trí Frontend Developer (Angular) tại công ty Digital Service Solutions (DSS)</h4>
+            <div class="form-apply">
+              <div class="form-item">
+                <label for="fullName" class="form-label">Tên của bạn <span>&#42;</span></label>
+                <input type="text" id="fullName" class="form-control" required>
+              </div>
+              
+              <div class="form-group">
+                <div class="form-item">
+                  <label for="email" class="form-label">Email <span>&#42;</span></label>
+                  <input type="email" id="email" class="form-control" required>
+                </div>
+
+                <div class="form-item">
+                  <label for="phone" class="form-label">Số điện thoại <span>&#42;</span></label>
+                  <input type="text" id="phone" class="form-control" required>
+                </div>
+              </div>
+
+              <div class="form-item">
+                <label for="address" class="form-label">Địa chỉ của bạn</label>
+                <input type="text" id="address" class="form-control">
+              </div>
+
+              <div class="form-item">
+                <input type="file" id="cv" class="form-control">
+              </div>
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-primary btn-exit" data-bs-dismiss="modal">Đóng</button>
+            <button type="submit" class="btn btn-primary btn-apply">Nộp CV</button>
+          </div>
+        </form>
       </div>
     </div>
   </div>
